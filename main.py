f660eee08f5715ab16002515502e78fcbaad0d1b
Dhruv Desai <dhruvdesai222@gmail.com>
2023-01-26 10:24:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Dhruv Desai
+# C0883865
+# Assignment 2
+
+print("My name is Dhruv \nI am in sem 1 \nI am in DOCT")
+
+print("Name of my courses are: Python, Operating System, Virtualization, Database Design, Big Data, Networking")
